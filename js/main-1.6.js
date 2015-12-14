1ed6be3ac7c4aaa8308c74324dbe5b56c1180515
Florent Jochaud <fjochaud@gmail.com>
2015-12-14 19:03:44 +0100

    clean up, link to rsvp
    clea

--- js/main-1.6.js
+++ js/main-1.6.js
@@ -46,6 +46,7 @@ https://github.com/imakewebthings/jquery-waypoints/blob/master/MIT-license.txt
 !function(e){if("function"==typeof define&&define.amd)define(e);else if("object"==typeof exports)module.exports=e();else{var n=window.Cookies,t=window.Cookies=e();t.noConflict=function(){return window.Cookies=n,t}}}(function(){function e(){for(var e=0,n={};e<arguments.length;e++){var t=arguments[e];for(var o in t)n[o]=t[o]}return n}function n(t){function o(n,r,i){var c;if(arguments.length>1){if(i=e({path:"/"},o.defaults,i),"number"==typeof i.expires){var s=new Date;s.setMilliseconds(s.getMilliseconds()+864e5*i.expires),i.expires=s}try{c=JSON.stringify(r),/^[\{\[]/.test(c)&&(r=c)}catch(a){}return r=t.write?t.write(r,n):encodeURIComponent(String(r)).replace(/%(23|24|26|2B|3A|3C|3E|3D|2F|3F|40|5B|5D|5E|60|7B|7D|7C)/g,decodeURIComponent),n=encodeURIComponent(String(n)),n=n.replace(/%(23|24|26|2B|5E|60|7C)/g,decodeURIComponent),n=n.replace(/[\(\)]/g,escape),document.cookie=[n,"=",r,i.expires&&"; expires="+i.expires.toUTCString(),i.path&&"; path="+i.path,i.domain&&"; domain="+i.domain,i.secure?"; secure":""].join("")}n||(c={});for(var p=document.cookie?document.cookie.split("; "):[],d=/(%[0-9A-Z]{2})+/g,u=0;u<p.length;u++){var f=p[u].split("="),l=f[0].replace(d,decodeURIComponent),m=f.slice(1).join("=");'"'===m.charAt(0)&&(m=m.slice(1,-1));try{if(m=t.read?t.read(m,l):t(m,l)||m.replace(d,decodeURIComponent),this.json)try{m=JSON.parse(m)}catch(a){}if(n===l){c=m;break}n||(c[l]=m)}catch(a){}}return c}return o.get=o.set=o,o.getJSON=function(){return o.apply({json:!0},[].slice.call(arguments))},o.defaults={},o.remove=function(n,t){o(n,"",e(t,{expires:-1}))},o.withConverter=n,o}return n(function(){})});
 
 
+
 function changeLang(lang) {
 	Cookies.set('lang', lang);
 	setLang(lang);
@@ -237,42 +238,6 @@ $(function() {
         }
       });
 
-	// nav_bzh_container.waypoint({
-	// 	handler: function(event, direction) {
-	// 		console.log(event);
-	// 		if (direction == 'up') {
-	//
-	// 			nav_bzh_container.css({
-	// 				'height': subnav.outerHeight(),
-	// 				'position': '',
-	// 				'bottom': ''
-	// 			});
-	// 			nav_bzh_container.stop().addClass("sticky").css("top", sub_top_spacing).animate({
-	// 				"top": sub_top_spacing
-	// 			});
-	//
-	// 		} else {
-	// 			nav_bzh_container.css({
-	// 				'position':'absolute',
-	// 				'bottom': '',
-	// 				'height': ''
-	// 			});
-	// 			nav_bzh_container.stop().removeClass("sticky").animate();
-	// 		}
-	//
-	// 	},
-	// 	offset: function() {
-	// 		return -nav_bzh_container.parent().parent().outerHeight();
-	// 	//	-sub_waypoint_offset;
-	// 	}
-	// });
-
-
-
-
-
-
-
   var all_nav_links = $("#nav ul li a, #bzh-nav ul li a, #az-nav ul li a");
 
   var topsections = $("section[id|='top']");
@@ -293,9 +258,6 @@ $(function() {
   })
 
 
-
-
-
   var subsections = $("section[id|='sub']");
   var subnavigation_links = $("#bzh-nav ul li a, #az-nav ul li a");
 
@@ -313,11 +275,6 @@ $(function() {
     },
     offset: '50%'
   })
-
-
-
-
-
 
 
   all_nav_links.click(function(event) {
